d14a71e37fa0b8e35c77c87f386d68af4e4194c0
Anasaksbi1 <anasalfilaali@gmail.com>
2023-10-30 14:27:46 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,53 +1,17 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
-#include <string.h>
-
+#include "main.h"
 /**
- * check_num - Checks if a string consists of digits
- * @str: The input string to check
- *
- * Return: 1 if all characters are digits, 0 otherwise
+ * free_grid - Frees a 2-dimensional grid
+ * @grid: The grid to be freed
+ * @height: The height of the grid
  */
-int check_num(char *str)
+void free_grid(int **grid, int height)
 {
-	for (int count = 0; str[count] != '\0'; count++)
-	{
-		if (!isdigit(str[count]))
-		{
-			return (0);
-		}
-	
-	return (1);
-}
+	int i;
 
-int main(int argc, char *argv[])
-{
-	int count;
-	int str_to_int;
-	int sum = 0;
-
-	if (argc < 2)
+	for (i = 0; i < height; i++)
 	{
-		printf("0\n");
-		return (0);
+		free(grid[i]);
 	}
 
-	for (count = 1; count < argc; count++)
-	{
-		if (check_num(argv[count]))
-		{
-			str_to_int = atoi(argv[count]);
-			sum += str_to_int;
-		}
-		else
-		{
-			printf("Error\n");
-			return (1);
-		}
-	}
-
-	printf("%d\n", sum);
-
-	return (0);
+	free(grid);
 }
