dcc923fa938cc4949a9164632c4162b462687a85
Anasaksbi1 <anasalfilaali@gmail.com>
2023-11-03 02:30:04 -0700

    1-string_nconcat.c

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,46 @@
+#include "main.h"
+/**
+ * string_nconcat - string concatenation function
+ * @s1: string to concatenate
+ * @s2: string to conc
+ * @n: number of characters to concat
+ * Return: string concatenation function
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int len1 = 0, len2 = 0;
+
+	unsigned int i = 0;
+
+	char *str;
+
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	while (s1[len1] != '\0')
+		len1++;
+	while (s2[len2] != '\0')
+		len2++;
+
+	if (len2 < n)
+		n = len2;
+
+	str = (char *)malloc(sizeof(char) * (len1 + n + 1));
+
+	if (str == NULL)
+		return (NULL);
+
+	while (i < len1 + n)
+	{
+		if (i >= len1)
+			str[i] = s2[i - len1];
+		else
+			str[i] = s1[i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
