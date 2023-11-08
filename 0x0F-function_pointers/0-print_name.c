39e8a3c1568ca729caa1fcd97afaa7203a25116f
Anasaksbi1 <anasalfilaali@gmail.com>
2023-11-08 14:36:41 -0800

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+/**
+ * print_name - print the name of the function
+ * @name: the name of the function
+ * @f: the function pointer
+*/
+void print_name(char *name, void (*f)(char *))
+{
+	if ((name != NULL) && (f != NULL))
+		f(name);
+}
